f7dbed320f5efd870b2252b6268fd7dce26363fd
Dallas Moore <moore.johndallas@gmail.com>
2023-08-04 20:00:44 -0500

    added logic for adding and getting from database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,21 +1,37 @@
 import { openDB } from 'idb';
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB('fate', 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains('fate')) {
+        console.log('fate database already exists');
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore('fate', { keyPath: 'id', autoIncrement: true });
+      console.log('fate database created');
     },
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const fateDb = await openDB('fate', 1);
+  const tx = fateDb.transaction('fate', 'readwrite');
+  const store = tx.objectStore('fate');
+  const request = store.put(content);
+  const result = await request;
+  console.log('putDb result', result);
+  return result;
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const fateDb = await openDB('fate', 1);
+  const tx = fateDb.transaction('fate', 'readonly');
+  const store = tx.objectStore('fate');
+  const request = store.getAll();
+  const result = await request;
+  console.log('getDb result', result);
+  return result;
+}
 
 initdb();
